62fa96f3cb8d55dfd275b31bdce1dec27cde9b0b
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-10 10:31:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,17 @@
 def sort_array_asc(array)
-  array.sort do |a,b|
+  array.sort do |a, b|
     a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a < b
+      1
+    elsif a == b
+      0
+    else
+      -1
+    end
   end
 end
